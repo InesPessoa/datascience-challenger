7de67de7800094ecfcd6872dafc72f0803a83457
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-31 21:57:00 +0000

    render question

--- ui/src/Pages/Home/index.tsx
+++ ui/src/Pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import {
+    Button,
     Container,
     Content,
     Footer,
@@ -12,28 +13,59 @@ import Navbar from '../../Components/Navbar/';
 import Question from '../../Components/Question';
 
 
-class Main extends Component<{}, {}> {
+interface IQuestion {
+    id: number;
+    groupId: number;
+    level: number;
+    question: string;
+    answers: string[];
+    correctAnswers: number[];
+    totalPoints: number;
+}
+interface IMainState {
+    runningQuest: boolean;
+    questions: IQuestion[];
+}
+class Main extends Component<{}, IMainState> {
     constructor(props: any) {
         super(props);
+        this.state = {
+            questions: [],
+            runningQuest: false,
+        };
+    }
+
+    public componentDidMount = () => {
+        fetch('http://localhost:5000/questions')
+            .then((res) => res.json())
+            .then((data: { questions: IQuestion[]}) => {
+                this.setState({ questions: data.questions });
+            });
     }
 
     public render() {
+        const { runningQuest, questions } = this.state;
         const question = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas erat diam, volutpat sit amet nunc eget, volutpat mollis urna. Nullam maximus euismod turpis, non viverra sapien. In hac habitasse platea dictumst. Etiam tempor nisi interdum nunc hendrerit interdum. In tincidunt odio in erat consequat, a consectetur dolor fringilla.';
         return (
             <Container>
                 <Header>
                     <Navbar />
                 </Header>
-                <Content style={{margin: '5% 10%'}}>
-                    <Question
-                        question={question}
-                        answers={['a', 'b', 'c', 'd']}
-                        correct_answers={[0, 1]}
-                    />
+                <Content style={{ margin: '5% 10%' }}>
+                    <Button onClick={this.startQuest} appearance="primary">Open Quiz</Button>
+                    {runningQuest && <Question
+                        question={questions[0].question}
+                        answers={questions[0].answers}
+                        correct_answers={questions[0].correctAnswers}
+                    />}
                 </Content>
                 <Footer>Footer</Footer>
             </Container>
         );
+    }
+
+    private startQuest = (event: any) => {
+        this.setState({ runningQuest: true });
     }
 }
 
