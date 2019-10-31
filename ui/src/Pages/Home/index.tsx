0bb7e8d17d98217cb2b8925d36aed234a31be383
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-31 21:06:36 +0000

    draft question

--- ui/src/Pages/Home/index.tsx
+++ ui/src/Pages/Home/index.tsx
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
 
-class Main extends Component<{}, {}> {
+import {
+    Button,
+    ButtonToolbar,
+    Checkbox,
+    CheckboxGroup,
+    Form,
+    FormGroup,
+    Message,
+} from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
+
+import Navbar from '../../Components/Navbar/';
+
+
+class Main extends Component<{}, { selectedAnswers: any }> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            selectedAnswers: ['A', 'C'],
+        };
+    }
+    public handleChange = (value: any) => {
+        this.setState({
+            selectedAnswers: value,
+        });
+    }
+
     public render() {
+        const { selectedAnswers } = this.state;
+        const question = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas erat diam, volutpat sit amet nunc eget, volutpat mollis urna. Nullam maximus euismod turpis, non viverra sapien. In hac habitasse platea dictumst. Etiam tempor nisi interdum nunc hendrerit interdum. In tincidunt odio in erat consequat, a consectetur dolor fringilla.';
         return (
-            <div>
-                Olá
-            </div>
+            <React.Fragment>
+                <Navbar />
+                <Form>
+                    <Message type="info" description={question} />
+                    <FormGroup>
+                        <CheckboxGroup
+                            inline={false}
+                            name="checkboxList"
+                            value={selectedAnswers}
+                            onChange={this.handleChange}
+                        >
+                            <Checkbox value="A">Item A</Checkbox>
+                            <Checkbox value="B">Item B</Checkbox>
+                            <Checkbox value="C">Item C</Checkbox>
+                            <Checkbox value="D">Item D</Checkbox>
+                        </CheckboxGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <ButtonToolbar>
+                            <Button appearance="primary">Submit</Button>
+                        </ButtonToolbar>
+                    </FormGroup>
+                </Form>
+            </React.Fragment>
         );
     }
 }
 
 export default Main;
+
+
+/*
+{
+    questions: [
+        {
+            question: 'abc?';
+            answers: [
+                'sfsdf',
+                'sdfsd',
+                'sfdf',
+                'ssgsdfs',
+            ],
+            right_answers: [1]
+        }
+    ]
+}*/
