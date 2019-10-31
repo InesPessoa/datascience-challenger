cecdba5e0624f3b5acf26f704a23487153caf41a
ines <ines@opplane.com>
2019-10-31 21:29:52 +0000

    endpoint with questions

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import json
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/questions', methods=["GET"])
+def get_questions():
+
+    file = open("resources/questions.json", "r")
+    questions = json.load(file)
+
+    return questions
 
 
 if __name__ == '__main__':
