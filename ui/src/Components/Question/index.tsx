0fe753753344ccd4177e6769f3b5380430faaed0
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-31 23:17:48 +0000

    control answers in question component

--- ui/src/Components/Question/index.tsx
+++ ui/src/Components/Question/index.tsx
@@ -14,30 +14,48 @@ import {
 interface IQuestionProps {
     question: string;
     answers: string[];
-    correct_answers: number[];
+    correctAnswers: number[];
 }
 interface IQuestionState {
-    selectedAnswers: string[];
+    selectedAnswers: number[];
+    success: boolean;
+    submitDisabled: boolean;
 }
 class Question extends Component<IQuestionProps, IQuestionState> {
     constructor(props: any) {
         super(props);
         this.state = {
             selectedAnswers: [],
+            submitDisabled: false,
+            success: undefined as any,
         };
     }
+
     public handleChange = (value: any) => {
         this.setState({
             selectedAnswers: value,
         });
     }
 
-    public render() {
+    public handleSubmit = (event: React.SyntheticEvent<Element, Event>) => {
         const { selectedAnswers } = this.state;
+        const comparison = this.props.correctAnswers.length === selectedAnswers.length
+            && this.props.correctAnswers.every((v) => selectedAnswers.indexOf(v) !== -1);
+        this.setState({ submitDisabled: true });
+        if (comparison) {
+            console.log('success!');
+        } else {
+            console.log('failed!');
+        }
+        event.preventDefault();
+    }
+
+    public render() {
+        const { selectedAnswers, submitDisabled } = this.state;
         const { question, answers } = this.props;
         return (
             <React.Fragment>
-                <Form style={{maxWidth: '60%', margin: '0% 20%'}}>
+                <Form style={{ maxWidth: '60%', margin: '0% 20%' }}>
                     <Message type="info" description={question} />
                     <br />
                     <FormGroup>
@@ -47,12 +65,18 @@ class Question extends Component<IQuestionProps, IQuestionState> {
                             value={selectedAnswers}
                             onChange={this.handleChange}
                         >
-                            {answers.map((answer) => <Checkbox key={answer} value={answer}>{answer}</Checkbox>)}
+                            {answers.map((answer, index) => <Checkbox key={index} value={index}>{answer}</Checkbox>)}
                         </CheckboxGroup>
                     </FormGroup>
                     <FormGroup>
                         <ButtonToolbar>
-                            <Button appearance="primary">Submit</Button>
+                            <Button
+                                appearance="primary"
+                                disabled={submitDisabled}
+                                onClick={this.handleSubmit}
+                            >
+                                Submit
+                            </Button>
                         </ButtonToolbar>
                     </FormGroup>
                 </Form>
