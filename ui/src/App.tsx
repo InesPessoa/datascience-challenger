c03b902e5cc3bc5dd617b0254abaaedf1e15dd9e
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-25 23:23:15 +0000

    improve navbar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,44 @@
 import React, { Component, Suspense } from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
+    BrowserRouter,
     Route,
+    Switch,
 } from 'react-router-dom';
+import {
+    Avatar,
+    Container,
+    Header,
+    Nav,
+    Navbar,
+} from 'rsuite';
 
 const Home = React.lazy(() => import('./Pages/Home/'));
 
 class App extends Component<{}, {}> {
     public render() {
         return (
-            <Router>
-                <Switch>
-                    <Route path="/">
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Home />
-                        </Suspense>
-                    </Route>
-                </Switch>
-            </Router>
+            <Container>
+                <Header>
+                    <Navbar>
+                        <Navbar.Header>
+                            <img src="/img/clown-fish.svg" height={60} style={{ padding: '5px 30px 5px 20px' }} />
+                            <span>Bem-vindo ao DataScience Challenger</span>
+                        </Navbar.Header>
+                        <Navbar.Body>
+                            <Nav pullRight={true} style={{ height: '60px' }}>
+                                <Avatar style={{ margin: '10px' }} circle={true} src="/img/user.jpg" />
+                            </Nav>
+                        </Navbar.Body>
+                    </Navbar>
+                </Header>
+                <BrowserRouter>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route exact={true} path="/" component={Home} />
+                        </Switch>
+                    </Suspense>
+                </BrowserRouter>
+            </Container>
         );
     }
 }
