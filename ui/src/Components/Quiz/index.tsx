6aeaa99060ebee3d656a6059e10c12101f11bc91
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-26 22:33:30 +0000

    load questions on quiz, not home

--- ui/src/Components/Quiz/index.tsx
+++ ui/src/Components/Quiz/index.tsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
-import { IQuestion } from '../../Pages/Home';
 import Question from '../Question';
 
 
+export interface IQuestion {
+    id: number;
+    groupId: number;
+    level: number;
+    question: string;
+    answers: string[];
+    correct_answers: number[];
+    total_points: number;
+}
 export interface IQuizContext {
     currentQuestionNumber: number;
     totalQuestions: number;
@@ -11,24 +19,34 @@ export interface IQuizContext {
 }
 export const QuizContext = React.createContext(
     {
-        currentQuestionNumber : 0,
+        currentQuestionNumber: 0,
         // tslint:disable-next-line: no-empty
         handleGoNextQuestion: () => { },
         totalQuestions: 0,
     },
 );
-
-interface IQuizProps {
+interface IQuizState extends IQuizContext {
+    loading: boolean;
     questions: IQuestion[];
 }
-class Quiz extends Component<IQuizProps, IQuizContext> {
+class Quiz extends Component<{}, IQuizState> {
     constructor(props: any) {
         super(props);
         this.state = {
             currentQuestionNumber: 0,
             handleGoNextQuestion: this.handleGoNextQuestion,
-            totalQuestions: this.props.questions.length,
+            loading: true,
+            questions: [],
+            totalQuestions: 0,
         };
+    }
+
+    public componentDidMount = () => {
+        fetch('http://localhost:5000/questions')
+            .then((res) => res.json())
+            .then((data: { questions: IQuestion[] }) => {
+                this.setState({ questions: data.questions, loading: false, totalQuestions: data.questions.length });
+            });
     }
 
     public handleGoNextQuestion = (event: React.SyntheticEvent<Element, Event>) => {
@@ -42,8 +60,10 @@ class Quiz extends Component<IQuizProps, IQuizContext> {
     }
 
     public render() {
-        const { questions } = this.props;
-        const { currentQuestionNumber } = this.state;
+        const { loading, currentQuestionNumber, questions } = this.state;
+        if (loading) {
+            return 'Loading...';
+        }
         return (
             <QuizContext.Provider value={this.state}>
                 <Question
--- ui/src/Pages/Home/index.tsx
+++ ui/src/Pages/Home/index.tsx
@@ -9,38 +9,19 @@ import 'rsuite/dist/styles/rsuite-default.css';
 const Quiz = React.lazy(() => import('../../Components/Quiz/'));
 
 
-export interface IQuestion {
-    id: number;
-    groupId: number;
-    level: number;
-    question: string;
-    answers: string[];
-    correct_answers: number[];
-    total_points: number;
-}
 interface IMainState {
     runningQuest: boolean;
-    questions: IQuestion[];
 }
 class Main extends Component<{}, IMainState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            questions: [],
             runningQuest: false,
         };
     }
 
-    public componentDidMount = () => {
-        fetch('http://localhost:5000/questions')
-            .then((res) => res.json())
-            .then((data: { questions: IQuestion[] }) => {
-                this.setState({ questions: data.questions });
-            });
-    }
-
     public render() {
-        const { runningQuest, questions } = this.state;
+        const { runningQuest } = this.state;
         const buttonCenter = {
             display: 'block',
             margin: 'auto',
@@ -56,7 +37,7 @@ class Main extends Component<{}, IMainState> {
             <Content style={{ margin: '5% 10%' }}>
                 <Content>
                     {!runningQuest && openQuizButton}
-                    {runningQuest && <Quiz questions={questions} />}
+                    {runningQuest && <Quiz />}
                 </Content>
             </Content>
         );
